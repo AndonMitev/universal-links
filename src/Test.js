310a70aae4b8f66d43c3baf0a511921ccfd9d429
AndonMitev <mitevandon94@gmail.com>
2022-12-07 18:37:00 +0200

    update test

--- src/Test.js
+++ src/Test.js
@@ -3,7 +3,7 @@ import QRCode from "react-qr-code";
 const Test = () => {
   return <div>
     <p>Hello Mobile app</p>
-    <QRCode value='https://benevolent-druid-e55bba.netlify.app/' size={150} />
+    <QRCode value='https://benevolent-druid-e55bba.netlify.app/test' size={150} />
   </div>
 }
 
