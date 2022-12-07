38d57eb74d79adf0487e072d63c1ae52c8dfa039
AndonMitev <mitevandon94@gmail.com>
2022-12-07 17:23:55 +0200

    fix publish

--- src/Test.js
+++ src/Test.js
@@ -1,23 +1,4 @@
-import axios from 'axios'
-import { useEffect } from 'react'
-import { usePolling } from './usePolling'
-
-const address = '0xecde133a5e73019e492b7c71640faaf0b20a7341'
-
 const Test = () => {
-  const { startPolling, stopPolling } = usePolling({
-    interval: 2000,
-    callback: () => axios.get(`https://test-nfts.ap.ngrok.io/inventory/get/items/${address}`),
-    onSuccess: (data) => {
-      console.log(data)
-
-      return true
-    },
-    shouldRun: false
-  })
-
-
-
 
 
   return <div>
