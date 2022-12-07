e04564c4cda7195d0cf86503bd0659c1f40aa8bd
AndonMitev <mitevandon94@gmail.com>
2022-12-07 18:52:57 +0200

    feat: update qr code link

--- src/Test.js
+++ src/Test.js
@@ -3,7 +3,7 @@ import QRCode from "react-qr-code";
 const Test = () => {
   return <div>
     <p>Hello Mobile app</p>
-    <QRCode value='https://benevolent-druid-e55bba.netlify.app/test' size={150} />
+    <QRCode value="https://1b1.art/claim?authToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uTmFtZSI6IjRFVkUgRXhjZXB0aW9uIiwidG9rZW5JZCI6OTM5NiwiaWF0IjoxNjUxNTYyNTgxLCJleHAiOjE3MTIwNDI1ODF9.jH0zwN4MMSi9qKhddgJpgk02sanTzwoB9_hffY7RC6E&collectionName=4EVE Exception&itemId=9396" size={150} />
   </div>
 }
 
