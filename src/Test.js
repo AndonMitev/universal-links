ceabb99ec45ae980ef75ce4bf16ff44852ff5a89
AndonMitev <mitevandon94@gmail.com>
2022-12-07 17:42:18 +0200

    add qr code

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,9 @@
+import QRCode from "react-qr-code";
+
 const Test = () => {
-
-
   return <div>
-    Hello Mobile app
+    <p>Hello Mobile app</p>
+    <QRCode value='https://benevolent-druid-e55bba.netlify.app/' size={150} />
   </div>
 }
 
