f36d52f28fd1760e796aac2631f770d87c199564
AndonMitev <mitevandon94@gmail.com>
2022-12-10 01:24:04 +0200

    very shit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 
-import { useEffect } from 'react';
-import { Routes, Route, Link } from "react-router-dom"
+// import { useEffect } from 'react';
+// import { Routes, Route, Link } from "react-router-dom"
 import './App.css';
-import Buy from './Buy';
-import Test from './Test';
+// import Buy from './Buy';
+// import Test from './Test';
 
 function App() {
-  useEffect(() => {
-    window.location = `https://mint.opn.network/claim?c=4EVE Exception`
-  }, [])
+  // useEffect(() => {
+  //   window.location = `https://mint.opn.network/claim?c=4EVE Exception`
+  // }, [])
 
   return (
     <div className="App">
       <div>
         <h1>This is the home page</h1>
-        <Link to="test">Test</Link>
-        <Link to="buy">Buy</Link>
+        {/* <Link to="test">Test</Link>
+        <Link to="buy">Buy</Link> */}
       </div>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Test />} />
         <Route path="/buy" element={<Buy />} />
         <Route path="/test" element={<Test />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
