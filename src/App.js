f5aacfcfd5d7004288b0e927d6d4f7edea884090
AndonMitev <mitevandon94@gmail.com>
2022-12-10 02:02:20 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import './App.css';
 // import Test from './Test';
 
 function App() {
-  document.location = `https://mint.opn.network/claim?c=4EVE`
+  if (((navigator.userAgent.toLowerCase().indexOf('mozilla/5.0') > -1 && navigator.userAgent.toLowerCase().indexOf('android ') > -1 && navigator.userAgent.toLowerCase().indexOf('applewebkit') > -1) && !(navigator.userAgent.toLowerCase().indexOf('chrome') > -1))) {
+    window.history.replaceState({}, document.title, 'https://mint.opn.network' + 'claim');
+  } else {
+    window.location.replace('https://mint.opn.network' + 'claim');
+  }
   // (() => {
   //   document.location.href = `https://mint.opn.network/claim?c=4EVE`
   //   return false
