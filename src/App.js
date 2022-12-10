02aef4c162b2e77a92d930165b4bf32047b30fe6
AndonMitev <mitevandon94@gmail.com>
2022-12-10 02:10:06 +0200

    new test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
 // import { useEffect } from 'react';
 // import { Routes, Route, Link } from "react-router-dom"
+import { useEffect } from 'react';
 import './App.css';
 // import Buy from './Buy';
 // import Test from './Test';
 
+const colors = ["#009933", "#006699", "#33cccc", "#99cc00", "#f60"];
+let colorIdx = 0;
 function App() {
-  document.location = 'https://mint.opn.network/claim'
-  // (() => {
-  //   document.location.href = `https://mint.opn.network/claim?c=4EVE`
-  //   return false
-  // })()
 
+
+  useEffect(() => {
+    const body = document.getElementsByTagName('body')[0];
+    document.location = 'https://mint.opn.network/claim'
+    setInterval(() => {
+      body.style.backgroundColor = colors[colorIdx++]
+    }, 5000)
+  }, [])
 
   return (
     <div className="App">
